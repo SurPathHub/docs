d0cc558ce95800900c3911388227bf518bd117df
prLorence <paulorencereyes@gmail.com>
2021-11-03 21:41:28 +0800

    Added dayong to the projects section

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,5 +4,11 @@ export const projects = [
         "description": "SurPath Hub's design system implementation for the web. Made with ❤, TypeScript, and SCSS",
         "link": "/docs/ugnay/",
         "img": "https://raw.githubusercontent.com/MatteuSan/SurPathHub.github.io/dev-gatsby-rewrite/src/images/ugnay-preview.png"
+    },
+    {
+        "name": "Dayong",
+        "description": "Discord Bot for SurPath Hub's server",
+        "link": "/docs/Dayong/",
+        "img": "https://raw.githubusercontent.com/MatteuSan/SurPathHub.github.io/dev-gatsby-rewrite/src/images/ugnay-preview.png"
     }
 ];
